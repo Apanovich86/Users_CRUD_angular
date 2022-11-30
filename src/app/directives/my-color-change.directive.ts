d32c5e6c77aa9e9c73bb36260c9640b62258261d
Apanovich86 <apanovich.anna.itec@gmail.com>
2022-11-30 23:55:38 +0200

    add directives

--- src/app/directives/my-color-change.directive.ts
+++ src/app/directives/my-color-change.directive.ts
@@ -3,13 +3,12 @@ import {Directive, ElementRef, Input, Renderer2} from '@angular/core';
 @Directive({
   selector: '[myColorChange]'
 })
+
 export class MyColorChangeDirective {
 
-  constructor(private elementRef: ElementRef, private renderer: Renderer2) {
+  constructor(private elementRef: ElementRef, private renderer: Renderer2) {}
 
-  }
-
-  @Input("myColorChange") set changeColor (color: string) {
+  @Input("myColorChange") set changeColor(color: string) {
     this.renderer.setStyle(this.elementRef.nativeElement, "color", color);
   }
 }
